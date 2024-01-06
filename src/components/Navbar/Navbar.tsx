9dd2d2bf148a846032632600965e8f059ab5bb7b
Jonas <jonasexplore@gmail.com>
2024-01-06 16:04:07 -0300

    feat(components): add logo on navbar

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,11 +1,22 @@
+"use client";
+
+import Image from "next/image";
+import { ModeToggle } from "../Toggle";
 import { Avatar } from "./components/Avatar";
+import logo from "../../../public/logo.svg";
 
 export const Navbar = () => {
   return (
     <div className="flex justify-between items-center ">
-      <span>reflectify</span>
+      <div className="flex gap-2 items-center">
+        <Image alt="logo" src={logo} className="w-4 h-4" />
+        <span className="font-bold">reflectify</span>
+      </div>
       <strong>Team Board</strong>
-      <Avatar />
+      <div className="flex gap-4">
+        <ModeToggle />
+        <Avatar />
+      </div>
     </div>
   );
 };
