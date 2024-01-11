dda92d93ea0959e76009743528d2be5fd77f0255
Jonas <jonasexplore@gmail.com>
2024-01-10 22:06:16 -0300

    feat(board): reset state

--- src/app/board/page.tsx
+++ src/app/board/page.tsx
@@ -1,7 +1,15 @@
 "use client";
 
+import { useEffect } from "react";
+
+import { useStoreBoard } from "../store";
+
 import { Board } from "./components/board";
 
 export default function Home() {
+  const { reset } = useStoreBoard();
+
+  useEffect(() => () => reset(), []);
+
   return <Board />;
 }
