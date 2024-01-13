28e2dfa29efd870e2feae21efc39ea11397f84be
Jonas <jonasexplore@gmail.com>
2024-01-12 21:01:38 -0300

    feat(api): create socket server

---
+++ src/app/api/socket/route.ts
@@ -0,0 +1,70 @@
+import { NextApiRequest } from "next";
+import { Server, Socket } from "socket.io";
+
+import { NextApiResponseWithSocket } from "@/app/types/http";
+
+const PORT = Number(process.env.SOCKET_PORT ?? 3005);
+
+export type ClientProps = {
+  id: string;
+  sender?: string;
+};
+
+const clients = new Map<Socket, ClientProps>(new Set());
+
+export async function GET(
+  _req: NextApiRequest,
+  res: NextApiResponseWithSocket
+) {
+  if (res?.socket?.server?.io) {
+    res.status(200).json({
+      success: true,
+      message: "Socket is already running",
+      socket: `:${PORT}`,
+    });
+    return;
+  }
+
+  console.log("Starting Socket.IO server on port:", PORT);
+
+  const io = new Server({
+    path: "/api/socket",
+    addTrailingSlash: false,
+    cors: { origin: "*" },
+  }).listen(PORT);
+
+  io.on("connect", (socket) => {
+    console.log("socket connect", socket.id);
+    socket.broadcast.emit("welcome", `Welcome ${socket.id}`);
+
+    const metadata = { id: socket.id };
+
+    clients.set(socket, metadata);
+
+    socket.on("message", (message: ClientProps) => {
+      const metadata = clients.get(socket);
+
+      if (!metadata) {
+        return;
+      }
+
+      message.sender = metadata?.id;
+
+      [...Array.from(clients.keys())].forEach((client) => {
+        client.emit("receive", message);
+      });
+    });
+
+    socket.on("disconnect", async () => {
+      clients.delete(socket);
+      console.log("socket disconnect");
+    });
+  });
+
+  res.socket.server.io = io;
+  res.status(201).json({
+    success: true,
+    message: "Socket is started",
+    socket: `:${PORT}`,
+  });
+}
