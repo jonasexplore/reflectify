0b408c9ca7e8753a6d5e966699af17bbaba87043
Jonas <jonasexplore@gmail.com>
2024-01-15 21:50:11 -0300

    feat(page): start homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,13 +4,42 @@ import { useRouter } from "next/navigation";
 
 import { Button } from "@/components/ui/button";
 
+import { ModeToggle } from "./boards/components/toggle";
+
 export default function Home() {
   const router = useRouter();
+
   return (
     <div className="h-screen w-full">
-      <div className="flex justify-between mx-6 py-4">
-        <div>Tela inicial</div>
-        <Button onClick={() => router.push("/auth")}>Entrar</Button>
+      <div className="mx-6 py-4">
+        <div className="flex justify-between ">
+          <span className="font-bold">reflectify</span>
+          <div className="flex gap-8 items-center">
+            <ul className="flex gap-6">
+              <li>Sobre</li>
+              <li>Benefícios</li>
+              <li>Planos</li>
+            </ul>
+            <ModeToggle />
+            <Button onClick={() => router.push("/auth")}>Entrar</Button>
+          </div>
+        </div>
+
+        <div>
+          <h2 className="text-lg font-bold mb-2">Planos</h2>
+
+          <div className="flex gap-4">
+            <div className="p-4 h-[300px] rounded-lg border w-full font-bold flex items-center justify-center">
+              Gratuito
+            </div>
+            <div className="p-4 h-[300px] rounded-lg border w-full text-muted flex items-center justify-center">
+              Em breve
+            </div>
+            <div className="p-4 h-[300px] rounded-lg border w-full text-muted flex items-center justify-center">
+              Em breve
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
