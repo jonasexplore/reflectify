991161d77fa062335f83f6166ee42a6594a043e8
Jonas <jonasexplore@gmail.com>
2024-01-20 21:51:06 -0300

    feat(services): create board service

---
+++ src/app/services/boards/index.ts
@@ -0,0 +1,11 @@
+import { api } from "../api";
+
+export const fetchBoard = async (userId: string) => {
+  try {
+    const output = await api.get("/boards", { params: { userId } });
+
+    return output.data?.boards;
+  } catch (error) {
+    return [];
+  }
+};
