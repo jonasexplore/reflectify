0e5164213749e4a018b6d8c7d8b739a43eee14b8
Jonas Brito <jonasexplore@gmail.com>
2024-02-21 20:43:22 -0300

    refactor(board): removes unused imports

--- src/app/boards/components/navbar/components/Avatar.tsx
+++ src/app/boards/components/navbar/components/Avatar.tsx
@@ -1,7 +1,6 @@
 "use client";
 
 import { ExitIcon } from "@radix-ui/react-icons";
-import Image from "next/image";
 import { signOut, useSession } from "next-auth/react";
 
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
