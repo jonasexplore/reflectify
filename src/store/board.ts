61a7e7d91975d05e1b0ad751ae7ffeb23e99f194
Jonas Brito <jonasexplore@gmail.com>
2024-04-02 18:58:28 -0300

    feat(store): remove unused methods

--- src/store/board.ts
+++ src/store/board.ts
@@ -21,105 +21,19 @@ type StoreProps = {
     replace?: boolean | undefined
   ) => void;
   socket: Socket | null;
-  board: Pick<BoardProps, "id" | "name">;
-  cards: CardProps[];
-  fillBoard: (values: BoardProps) => void;
-  setCards: (values: CardProps[]) => void;
-  addCard: (containerId: UniqueIdentifier, card: CardProps) => void;
-  removeCard: (containerId: UniqueIdentifier, cardId: UniqueIdentifier) => void;
-  containers: ContainerProps[];
-  setContainers: (values: ContainerProps[]) => void;
-  removeContainer: (containerId: UniqueIdentifier) => void;
+  board: Pick<BoardProps, "id" | "name" | "userId">;
   items: ItemsProps;
-  setItems: (values: ItemsProps) => void;
+  cards: CardProps[];
+  containers: ContainerProps[];
   containersIds: UniqueIdentifier[];
-  setContainersIds: (values: UniqueIdentifier[]) => void;
-  reset: () => void;
 };
 
 export const useStoreBoard = create<StoreProps>((set) => ({
   set,
   socket: null,
-  board: { id: "", name: "" },
-  cards: [],
-  setCards: (value) => set({ cards: value }),
-  fillBoard: (value) =>
-    set((state) => {
-      const containersIds = value.columns.map((column) => column.id);
-      const cards = value.columns.reduce<CardProps[]>(
-        (acc, curr) => acc.concat(curr.cards),
-        []
-      );
-      const containers = [
-        ...state.containers,
-        ...value.columns.map((column) => ({
-          color: "red",
-          id: column.id,
-          name: column.name,
-        })),
-      ];
-      const items = Object.assign(
-        state.items,
-        value.columns.reduce(
-          (acc, curr) =>
-            Object.assign(acc, {
-              [curr.id]: curr.cards.map((card) => card.id),
-            }),
-          {}
-        )
-      );
-
-      return {
-        items,
-        cards,
-        containers,
-        containersIds,
-      };
-    }),
-  addCard: (containerId, card) => {
-    set((state) => {
-      const items = state.items;
-
-      return {
-        cards: [...state.cards, card],
-        items: { ...items, [containerId]: [...items[containerId], card.id] },
-      };
-    });
-  },
-  removeCard: (containerId, cardId) =>
-    set((state) => {
-      const items = state.items;
-
-      return {
-        cards: state.cards.filter((card) => card.id !== cardId),
-        items: {
-          ...items,
-          [containerId]: items[containerId].filter((id) => id !== cardId),
-        },
-      };
-    }),
-  containers: [],
-  setContainers: (value) => set({ containers: value }),
+  board: { id: "", name: "", userId: "" },
   items: {},
-  setItems: (value) => set({ items: value }),
+  cards: [],
+  containers: [],
   containersIds: [],
-  setContainersIds: (value) => set({ containersIds: value }),
-  removeContainer: (containerId) =>
-    set((state) => {
-      const items = state.items;
-      delete items[containerId];
-
-      return {
-        items,
-        containers: state.containers.filter((item) => item.id !== containerId),
-        containersIds: state.containersIds.filter((id) => id !== containerId),
-      };
-    }),
-  reset: () =>
-    set({
-      items: {},
-      cards: [],
-      containers: [],
-      containersIds: [],
-    }),
 }));
