bcd2286168cab6cd98bd082782eeaccc04cdbe62
Jonas Brito <jonasexplore@gmail.com>
2024-04-23 08:16:22 -0300

    fix(auth): add secret variable to next auth

--- src/app/api/auth/options.ts
+++ src/app/api/auth/options.ts
@@ -1,10 +1,12 @@
+import { AuthOptions } from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
 
-export const authOptions = {
+export const authOptions: AuthOptions = {
   providers: [
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID ?? "",
       clientSecret: process.env.GOOGLE_CLIENT_SECRET ?? "",
     }),
   ],
+  secret: process.env.SECRET,
 };
