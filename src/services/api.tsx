06b46516a7ec0477fbf6a5ebbc0ea0069e2a793d
Jonas Brito <jonasexplore@gmail.com>
2024-04-23 08:27:57 -0300

    fix(services): adjusts base url

--- src/services/api.tsx
+++ src/services/api.tsx
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: "http://localhost:3000/api",
+  baseURL: process.env.API_URL,
   headers: { "Content-Type": "application/json" },
 });
